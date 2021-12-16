cccc3811063efecf9c7c43817e49ad7dc98b1dd5
斌 MAC <1170323375@qq.com>
2021-12-16 19:44:30 +0800

    没事啥就提交触发 git action

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,7 +3,7 @@ import { checkAuth } from './login/helper'
 import toastRegistry from "./plugins/toast/index";
 
 export default ({
-  Vue, // VuePress 正在使用的 Vue 构造函数
+  Vue, // VuePress 正在使用的 Vue 构造函数?
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
   siteData // 站点元数据
